a77639e13732c61d65a543069536da3ed467cdc3
Yannick Vanderstraeten <yanvnd@telesoftas.com>
2021-03-19 18:35:15 +0200

    Add file with Bankomat assignment

---
+++ calculator/Classes/Assignment.swift
@@ -0,0 +1,25 @@
+//
+//  Assignment.swift
+//  calculator
+//
+//  Created by Yannick Vanderstraeten on 2021-03-19.
+//
+
+import Foundation
+
+//
+//  Requirement:
+//  Create a simple bankomat app
+//
+//  Two ways a user can interact:
+//  1.  Put in money
+//  2.  Extract money, giving the user the least amount of banknotes necessary with banknotes that are available
+//  2b. Additional after 1 and 2 are finished: Allow the user to choose "Prefer small banknotes"
+//
+//  When user extracts money, display in console (print) which notes were given to him (which value and how many)
+//  eg. Based on available banknotes, if user extracts 100, possible outputs:
+//  "50: 2"
+//  "50: 1, 20: 2, 10: 1"
+//
+//  Don't worry about the formatting, print it out in any form you want
+//
